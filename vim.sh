247fdf1f1a506d86d14b860814b65166c2580ac1
Dermot A. Harris <dermot.harris@teamaol.com>
2015-05-09 21:15:12 -0400

    Added vim.sh to install the vim plugins I like

---
+++ vim.sh
@@ -0,0 +1,11 @@
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+
+git clone https://github.com/godlygeek/tabular.git
+
+#vim-snipmate and dependencies
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
+# Optional:
+git clone https://github.com/honza/vim-snippets.git
